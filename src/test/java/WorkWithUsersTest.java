770a2fe5a0190f77190f2830daef657db6e55894
Alexander <sashulik999@gmail.com>
2020-07-21 17:14:20 +0300

    Update WorkWithUsersTest.java

--- src/test/java/WorkWithUsersTest.java
+++ src/test/java/WorkWithUsersTest.java
@@ -1,27 +1,27 @@
-import auxiliary.WorkWithUsers;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+// import auxiliary.WorkWithUsers;
+// import org.junit.jupiter.api.Assertions;
+// import org.junit.jupiter.api.Test;
 
-public class WorkWithUsersTest {
-    @Test
-    public void testIsUserRegistered(){
-//         boolean expected = true;
+// public class WorkWithUsersTest {
+//     @Test
+//     public void testIsUserRegistered(){
+// //         boolean expected = true;
 
-//         Assertions.assertEquals(WorkWithUsers.isUserRegistered("admin"), expected);
-    }
+// //         Assertions.assertEquals(WorkWithUsers.isUserRegistered("admin"), expected);
+//     }
 
-    @Test
-    public void testIsAuthDataValid(){
-//         boolean expected = false;
+//     @Test
+//     public void testIsAuthDataValid(){
+// //         boolean expected = false;
 
-//         Assertions.assertEquals(WorkWithUsers.isAuthDataValid("admin", "62734"), expected);
-    }
+// //         Assertions.assertEquals(WorkWithUsers.isAuthDataValid("admin", "62734"), expected);
+//     }
 
-    @Test
-    public void testFindForbiddenSymbols(){
-//         String expected = "Your login mustn't contains symbols & and #!";
-//         String actual = WorkWithUsers.findForbiddenSymbols("#admin");
+//     @Test
+//     public void testFindForbiddenSymbols(){
+// //         String expected = "Your login mustn't contains symbols & and #!";
+// //         String actual = WorkWithUsers.findForbiddenSymbols("#admin");
 
-//         Assertions.assertTrue(expected.equals(actual));
-    }
-}
+// //         Assertions.assertTrue(expected.equals(actual));
+//     }
+// }
